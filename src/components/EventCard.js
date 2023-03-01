62abe4379b0ee3a3b8493eaed42922fe7a87c793
dseydahmetova <dseydahmetova@gmail.com>
2023-03-01 01:41:17 -0500

    main page of app

---
+++ src/components/EventCard.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import { stocks as data } from "../event-api"
+
+function Dashboard() {
+ 
+
+  return (
+    <div className='dashboard'>
+      <h1>Most Active Stocks</h1>
+    
+
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Company Name</th>
+            <th>Price</th>
+            <th>Change</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+          {data.map(stock => (
+            <tr>
+              <td key={stock.name}>
+                <Link key={stock.symbol} to={`/stocks/${stock.symbol}`} >
+                  {stock.name}
+                </Link>
+              </td>
+              <td key={stock.lastPrice}>
+                <Link key={stock.symbol} to={`/stocks/${stock.symbol}`} >
+                  {stock.lastPrice}
+                </Link></td>
+              <td key={stock.change} className={stock.change > 0 ? 'green' : 'red'}>
+                <Link key={stock.symbol} to={`/stocks/${stock.symbol}`} >
+                  {stock.change}
+                </Link>
+              </td>
+            </tr>
+          ))
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Dashboard
+
+
